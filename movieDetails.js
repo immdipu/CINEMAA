9bfe08bb3a18a33bef045ec5800bcacac6244196
Dipu <dipuchaurasiya91@gmail.com>
2022-06-16 08:38:29 +0545

    New brach

--- movieDetails.js
+++ movieDetails.js
@@ -1,4 +1,9 @@
 
+const posterBig = document.querySelector('.poster_big')
+const movieDetailnavContainer = document.querySelector('.movieDetailnavContainer')
+const gradient = document.querySelector('.gradient')
+const posterBigImg = document.querySelector('.poster_big_img')
+const posterBBig = document.querySelector('.posterbig');
 
 const arrowLeft = document.querySelector('.arrow_left');
 const hamburger = document.querySelector('.hamburger');
@@ -8,9 +13,18 @@ const rightarrow = document.querySelector('.rightarrow');
 const lightDarkmode = document.querySelector('.light_darkmode');
 const movieDetails = document.querySelector('.movie_details');
 const sectionStory = document.querySelector('.section_story');
-const posterBig = document.querySelector('.poster_big');
+const movieDetailsAboutCategoryUl = document.querySelector('.movie_details_about_category_ul');
 
 
+window.addEventListener('scroll', function () {
+    let intiCon = posterBBig.getBoundingClientRect();
+    if (window.scrollY > intiCon.height - 150) {
+        movieDetailnavContainer.classList.add('bgadd');
+    }
+    else {
+        movieDetailnavContainer.classList.remove('bgadd');
+    }
+})
 
 
 lightDarkmode.addEventListener('click', function () {
@@ -31,7 +45,6 @@ const myApi = "6b2dec73b6697866a50cdaef60ccffcb"
 const NowPlaying = async () => {
     const res = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${myApi}&language=en-US&page=1`)
     const data = await res.json();
-    console.log(data);
     const NowPlayingmovies = data.results;
     return NowPlayingmovies;
 }
@@ -39,7 +52,7 @@ const NowPlaying = async () => {
 //<a class="posterlink" href="./movieDetail.html"></a>//
 const NowPlayingfun = (movie) => {
     return `<div class="Now_playing_movies" >
-    <a class="posterlink" href="./movieDetail.html"> <img class="poster" data-id="${movie.id}" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="${movie.title}"></a>
+    <a class="posterlink" href="#sideNavCon"> <img class="poster" data-id="${movie.id}" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="${movie.title}"></a>
          <p class="movie_title">${movie.title}</p>
          <div class="date_rating">
              <p class="date">${dateFormatter(movie.release_date)}</p><span class="dot dot2"></span>
@@ -58,6 +71,9 @@ const dateFormatter = function (date) {
     const newDate = currdate.slice(0, 4)
     return newDate;
 }
+
+
+
 
 
 
@@ -111,38 +127,78 @@ leftArrow.addEventListener('click', prevSlide);
 rightarrow.addEventListener('click', nextSlide);
 
 
-
 /* MOVIE CLCIKED*/
 
-const html2 = function (movie) {
+let cate = ""
+const html2 = function (moviee) {
+    moviee.genres.forEach(item => {
+        cate += `<li class="movie_details_category_ul_li">${item.name}</li>`
+    })
 
-    return ` <div class="movie_details">
-    <img class="movie_details_poster" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="">
+    return `<div class="movie_details">
+    <img class="movie_details_poster" src="https://image.tmdb.org/t/p/w500/${moviee.poster_path}" alt="title">
     <div class="movie_details_about">
-        <h2 class="movie_details_title">${movie.title}</h2>
+        <h2 class="movie_details_title">${moviee.title}</h2>
         <div class="movie_details_about_category">
             <ul class="movie_details_about_category_ul">
-                <li class="movie_details_category_ul_li">Fantasy</li>
-                <li class="movie_details_category_ul_li">Adventure</li>
-                <li class="movie_details_category_ul_li">Action</li>
+            <li class="movie_details_category_ul_li">${moviee.genres[0].name}</li>
+            <li class="movie_details_category_ul_li">${moviee.genres[1].name}</li>
             </ul>
         </div>
         <div class="date_rating">
-            <p class="time">${movie.runtime}</p><span class="dot dot2"></span>
-            <p class="date">${movie.release_date}</p><span class="dot dot2"></span>
-            <p class="rating">${movie.vote_average}<span><svg xmlns="http://www.w3.org/2000/svg" width="10"
+            <p class="time">${moviee.runtime}</p><span class="dot dot2"></span>
+            <p class="date">${moviee.release_date}</p><span class="dot dot2"></span>
+            <p class="rating">${moviee.vote_average}<span><svg xmlns="http://www.w3.org/2000/svg" width="10"
                         height="10" fill="Yellow" class="star bi-star-fill" viewBox="0 0 16 16">
                         <path
                             d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
                     </svg></span></p>
         </div>
-        <button class="play_btn"><svg xmlns="http://www.w3.org/2000/svg" width="19" height="19"
+        <a class="playLink" href="https://www.2embed.ru/embed/imdb/movie?id=${moviee.imdb_id}"><button class="play_btn"><svg xmlns="http://www.w3.org/2000/svg" width="19" height="19"
                 fill="currentColor" class="path_btn bi-play-fill" viewBox="0 0 16 16">
                 <path class="path_btnn"
                     d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z">
                 </path>
-            </svg>Play</button>
+            </svg>Play</button></a> 
     </div>
 
 </div> `
 }
+
+const Bigposter = function (movieee) {
+    return `<img class="poster_big_img" src="https://image.tmdb.org/t/p/original/${movieee.backdrop_path}" alt="">`
+}
+
+
+
+const CurrMovie = async (id) => {
+    const res = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${myApi}`)
+    const data = await res.json();
+    return data;
+}
+
+
+let cath = ""
+
+
+const movieId = function (e) {
+    let ele = e.target;
+    if (ele.classList.contains('poster')) {
+        let id = ele.dataset.id;
+        CurrMovie(id).then((dat) => {
+            let htm = ""
+            htm = html2(dat)
+            movieDetails.innerHTML = htm;
+            let BigPoster = Bigposter(dat);
+            posterBBig.innerHTML = BigPoster;
+            sectionStory.textContent = dat.overview;
+        })
+
+    }
+}
+
+NowPlayingMoviesDiv.addEventListener('click', movieId)
+
+
+/*let myurl = "./movieDetail.html"
+window.location.replace(myurl)*/
