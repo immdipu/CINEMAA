be975d0112970e1048a323dda4a428cf52f2a229
Dipu <dipuchaurasiya91@gmail.com>
2022-06-16 14:30:36 +0545

    all code formatted

--- movieDetails.js
+++ movieDetails.js
@@ -1,115 +1,111 @@
+const posterBig = document.querySelector(".poster_big");
+const movieDetailnavContainer = document.querySelector(
+    ".movieDetailnavContainer"
+);
+const gradient = document.querySelector(".gradient");
+const posterBigImg = document.querySelector(".poster_big_img");
+const posterBBig = document.querySelector(".posterbig");
 
-const posterBig = document.querySelector('.poster_big')
-const movieDetailnavContainer = document.querySelector('.movieDetailnavContainer')
-const gradient = document.querySelector('.gradient')
-const posterBigImg = document.querySelector('.poster_big_img')
-const posterBBig = document.querySelector('.posterbig');
+const arrowLeft = document.querySelector(".arrow_left");
+const hamburger = document.querySelector(".hamburger");
+const NowPlayingMoviesDiv = document.querySelector(".Now_playing_movies_div");
+const leftArrow = document.querySelector(".leftarrow");
+const rightarrow = document.querySelector(".rightarrow");
+const lightDarkmode = document.querySelector(".light_darkmode");
+const movieDetails = document.querySelector(".movie_details");
+const sectionStory = document.querySelector(".section_story");
+const movieDetailsAboutCategoryUl = document.querySelector(
+    ".movie_details_about_category_ul"
+);
+const hamburgerPhone = document.querySelector(".hamburgerphone");
+const sidenavContainer = document.querySelector(".sidenav_container");
+const overlaySideNavabar = document.querySelector(".overlay_side_navabar");
+const sidenav = document.querySelector(".sidenav");
 
-const arrowLeft = document.querySelector('.arrow_left');
-const hamburger = document.querySelector('.hamburger');
-const NowPlayingMoviesDiv = document.querySelector('.Now_playing_movies_div');
-const leftArrow = document.querySelector('.leftarrow');
-const rightarrow = document.querySelector('.rightarrow');
-const lightDarkmode = document.querySelector('.light_darkmode');
-const movieDetails = document.querySelector('.movie_details');
-const sectionStory = document.querySelector('.section_story');
-const movieDetailsAboutCategoryUl = document.querySelector('.movie_details_about_category_ul');
-const hamburgerPhone = document.querySelector('.hamburgerphone');
-const sidenavContainer = document.querySelector('.sidenav_container');
-const overlaySideNavabar = document.querySelector('.overlay_side_navabar');
-const sidenav = document.querySelector('.sidenav');
+hamburgerPhone.addEventListener("click", function () {
+    sidenavContainer.classList.add("sidenav_container_active");
+    overlaySideNavabar.classList.add("sidenav_container_active");
+    hamburgerPhone.classList.add("hamburgerphonedeactive");
+});
+overlaySideNavabar.addEventListener("click", function () {
+    sidenavContainer.classList.remove("sidenav_container_active");
+    overlaySideNavabar.classList.remove("sidenav_container_active");
+    document.body.classList.remove("minimize_siderbar");
+    hamburgerPhone.classList.remove("hamburgerphonedeactive");
+});
 
-
-
-
-
-hamburgerPhone.addEventListener('click', function () {
-    sidenavContainer.classList.add('sidenav_container_active');
-    overlaySideNavabar.classList.add('sidenav_container_active');
-    hamburgerPhone.classList.add('hamburgerphonedeactive');
-
-})
-overlaySideNavabar.addEventListener('click', function () {
-    sidenavContainer.classList.remove('sidenav_container_active');
-    overlaySideNavabar.classList.remove('sidenav_container_active');
-    document.body.classList.remove('minimize_siderbar');
-    hamburgerPhone.classList.remove('hamburgerphonedeactive');
-})
-
-
-window.addEventListener('scroll', function () {
+window.addEventListener("scroll", function () {
     let intiCon = posterBBig.getBoundingClientRect();
     if (window.scrollY > intiCon.height - 150) {
-        movieDetailnavContainer.classList.add('bgadd');
+        movieDetailnavContainer.classList.add("bgadd");
+    } else {
+        movieDetailnavContainer.classList.remove("bgadd");
     }
-    else {
-        movieDetailnavContainer.classList.remove('bgadd');
-    }
-})
+});
 
+lightDarkmode.addEventListener("click", function () {
+    document.body.classList.toggle("light");
+});
 
-lightDarkmode.addEventListener('click', function () {
-    document.body.classList.toggle('light');
-})
+arrowLeft.addEventListener("click", function () {
+    document.body.classList.remove("minimize_siderbar");
+});
 
-arrowLeft.addEventListener('click', function () {
-    document.body.classList.remove('minimize_siderbar');
-})
+hamburger.addEventListener("click", function () {
+    document.body.classList.add("minimize_siderbar");
+});
 
-
-hamburger.addEventListener('click', function () {
-    document.body.classList.add('minimize_siderbar');
-})
-
-const myApi = "6b2dec73b6697866a50cdaef60ccffcb"
+const myApi = "6b2dec73b6697866a50cdaef60ccffcb";
 
 const NowPlaying = async () => {
-    const res = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${myApi}&language=en-US&page=1`)
+    const res = await fetch(
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=${myApi}&language=en-US&page=1`
+    );
     const data = await res.json();
     const NowPlayingmovies = data.results;
     return NowPlayingmovies;
-}
+};
 
 //<a class="posterlink" href="./movieDetail.html"></a>//
 const NowPlayingfun = (movie) => {
     let url = "./movieDetail.html?id=" + encodeURIComponent(movie.id);
     return `<div class="Now_playing_movies" >
-    <a class="posterlink" href="${url}"> <img class="poster" data-id="${movie.id}" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="${movie.title}"></a>
+    <a class="posterlink" href="${url}"> <img class="poster" data-id="${movie.id
+        }" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="${movie.title
+        }"></a>
          <p class="movie_title">${movie.title}</p>
          <div class="date_rating">
-             <p class="date">${dateFormatter(movie.release_date)}</p><span class="dot dot2"></span>
-             <p class="rating">${movie.vote_average}<span><svg xmlns="http://www.w3.org/2000/svg" width="10"
+             <p class="date">${dateFormatter(
+            movie.release_date
+        )}</p><span class="dot dot2"></span>
+             <p class="rating">${movie.vote_average
+        }<span><svg xmlns="http://www.w3.org/2000/svg" width="10"
                          height="10" fill="Yellow" class="star bi-star-fill" viewBox="0 0 16 16">
                          <path
                              d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
                      </svg></span></p>
              <div class="category">Movie</div>
              </div>
-         </div>`
-}
+         </div>`;
+};
 
 const dateFormatter = function (date) {
     let currdate = date;
-    const newDate = currdate.slice(0, 4)
+    const newDate = currdate.slice(0, 4);
     return newDate;
-}
+};
 
-
-
-
-NowPlaying().then(movies => {
-    movies.forEach(moviee => {
+NowPlaying().then((movies) => {
+    movies.forEach((moviee) => {
         const htmll = NowPlayingfun(moviee);
-        NowPlayingMoviesDiv.insertAdjacentHTML("beforeend", htmll)
+        NowPlayingMoviesDiv.insertAdjacentHTML("beforeend", htmll);
+    });
 
-    })
-
-    const NowPlayingMovies = document.querySelectorAll('.Now_playing_movies');
-    NowPlayingMovies.forEach((ele, i) => (ele.style.transform = `TranslateX(${i * 122}%)`));
-
-})
-
-
+    const NowPlayingMovies = document.querySelectorAll(".Now_playing_movies");
+    NowPlayingMovies.forEach(
+        (ele, i) => (ele.style.transform = `TranslateX(${i * 122}%)`)
+    );
+});
 
 // MOVIES SLIDER
 
@@ -117,52 +113,48 @@ let currSlide = 0;
 const maxSlide = 20;
 
 const gotoSlide = function (slides) {
-    const NowPlayingMovies = document.querySelectorAll('.Now_playing_movies');
-    NowPlayingMovies.forEach((ele, i) => (ele.style.transform = `TranslateX(${122 * (i - slides)}%)`));
-
-}
+    const NowPlayingMovies = document.querySelectorAll(".Now_playing_movies");
+    NowPlayingMovies.forEach(
+        (ele, i) => (ele.style.transform = `TranslateX(${122 * (i - slides)}%)`)
+    );
+};
 
 const nextSlide = function () {
     if (currSlide === maxSlide - 6) {
         /*currSlide = 0;*/
-        rightarrow.classList.add('disablemouse')
-        leftArrow.classList.remove('disablemouse')
-    }
-    else {
-        rightarrow.classList.remove('disablemouse')
-        leftArrow.classList.remove('disablemouse')
-        currSlide++
+        rightarrow.classList.add("disablemouse");
+        leftArrow.classList.remove("disablemouse");
+    } else {
+        rightarrow.classList.remove("disablemouse");
+        leftArrow.classList.remove("disablemouse");
+        currSlide++;
     }
     gotoSlide(currSlide);
-}
-
+};
 
 const prevSlide = function () {
     if (currSlide === 0) {
         /* currSlide = maxSlide - 6;*/
-        rightarrow.classList.remove('disablemouse')
-        leftArrow.classList.add('disablemouse')
-    }
-    else {
-        leftArrow.classList.remove('disablemouse')
-        rightarrow.classList.remove('disablemouse')
+        rightarrow.classList.remove("disablemouse");
+        leftArrow.classList.add("disablemouse");
+    } else {
+        leftArrow.classList.remove("disablemouse");
+        rightarrow.classList.remove("disablemouse");
         currSlide--;
-
     }
-    gotoSlide(currSlide)
+    gotoSlide(currSlide);
 };
 
-leftArrow.addEventListener('click', prevSlide);
-rightarrow.addEventListener('click', nextSlide);
-
+leftArrow.addEventListener("click", prevSlide);
+rightarrow.addEventListener("click", nextSlide);
 
 /* MOVIE CLCIKED*/
 
 const html2 = function (moviee) {
-    let cate = ""
-    moviee.genres.forEach(item => {
-        cate += `<li class="movie_details_category_ul_li">${item.name}</li>`
-    })
+    let cate = "";
+    moviee.genres.forEach((item) => {
+        cate += `<li class="movie_details_category_ul_li">${item.name}</li>`;
+    });
     return `<div class="movie_details">
     <img class="movie_details_poster" src="https://image.tmdb.org/t/p/w500/${moviee.poster_path}" alt="title">
     <div class="movie_details_about">
@@ -189,64 +181,52 @@ const html2 = function (moviee) {
             </svg>Play</button></a> 
     </div>
 
-</div> `
-}
+</div> `;
+};
 
 const Bigposter = function (movieee) {
-    return `<img class="poster_big_img" src="https://image.tmdb.org/t/p/original/${movieee.backdrop_path}" alt="">`
-}
-
-
+    return `<img class="poster_big_img" src="https://image.tmdb.org/t/p/original/${movieee.backdrop_path}" alt="">`;
+};
 
 const CurrMovie = async (id) => {
-    const res = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${myApi}`)
+    const res = await fetch(
+        `https://api.themoviedb.org/3/movie/${id}?api_key=${myApi}`
+    );
     const data = await res.json();
     return data;
-}
-
-
+};
 
 window.onload = function () {
     let url = document.location.href;
-    let fetcid = url.slice(url.indexOf('=') + 1)
+    let fetcid = url.slice(url.indexOf("=") + 1);
     CurrMovie(fetcid).then((dat) => {
-        let htm = ""
-        htm = html2(dat)
+        let htm = "";
+        htm = html2(dat);
         movieDetails.innerHTML = htm;
         let BigPoster = Bigposter(dat);
         posterBBig.innerHTML = BigPoster;
         sectionStory.textContent = dat.overview;
-    })
-}
+    });
+};
 
-
-
-
-
-
-let cath = ""
-
+let cath = "";
 
 const movieId = function (e) {
     let ele = e.target;
-    if (ele.classList.contains('poster')) {
+    if (ele.classList.contains("poster")) {
         let id = ele.dataset.id;
         CurrMovie(id).then((dat) => {
-            let htm = ""
-            htm = html2(dat)
+            let htm = "";
+            htm = html2(dat);
             movieDetails.innerHTML = htm;
             let BigPoster = Bigposter(dat);
             posterBBig.innerHTML = BigPoster;
             sectionStory.textContent = dat.overview;
-        })
-
+        });
     }
-}
+};
 
-
-
-NowPlayingMoviesDiv.addEventListener('click', movieId)
-
+NowPlayingMoviesDiv.addEventListener("click", movieId);
 
 /*let myurl = "./movieDetail.html"
 window.location.replace(myurl)*/
--- scriptl.js
+++ scriptl.js
@@ -1,97 +1,91 @@
+const arrowLeft = document.querySelector(".arrow_left");
+const hamburger = document.querySelector(".hamburger");
+const NowPlayingMoviesDiv = document.querySelector(".Now_playing_movies_div");
+const leftArrow = document.querySelector(".leftarrow");
+const rightarrow = document.querySelector(".rightarrow");
+const lightDarkmode = document.querySelector(".light_darkmode");
+const movieDetails = document.querySelector(".movie_details");
+const sectionStory = document.querySelector(".section_story");
+const posterBig = document.querySelector(".poster_big");
+const hamburgerPhone = document.querySelector(".hamburgerphone");
+const sidenavContainer = document.querySelector(".sidenav_container");
+const overlaySideNavabar = document.querySelector(".overlay_side_navabar");
+const sidenav = document.querySelector(".sidenav");
 
-const arrowLeft = document.querySelector('.arrow_left');
-const hamburger = document.querySelector('.hamburger');
-const NowPlayingMoviesDiv = document.querySelector('.Now_playing_movies_div');
-const leftArrow = document.querySelector('.leftarrow');
-const rightarrow = document.querySelector('.rightarrow');
-const lightDarkmode = document.querySelector('.light_darkmode');
-const movieDetails = document.querySelector('.movie_details');
-const sectionStory = document.querySelector('.section_story');
-const posterBig = document.querySelector('.poster_big');
-const hamburgerPhone = document.querySelector('.hamburgerphone');
-const sidenavContainer = document.querySelector('.sidenav_container');
-const overlaySideNavabar = document.querySelector('.overlay_side_navabar');
-const sidenav = document.querySelector('.sidenav');
+lightDarkmode.addEventListener("click", function () {
+    document.body.classList.toggle("light");
+});
 
+arrowLeft.addEventListener("click", function () {
+    document.body.classList.remove("minimize_siderbar");
+});
 
+hamburger.addEventListener("click", function () {
+    document.body.classList.add("minimize_siderbar");
+});
 
+hamburgerPhone.addEventListener("click", function () {
+    sidenavContainer.classList.add("sidenav_container_active");
+    overlaySideNavabar.classList.add("sidenav_container_active");
+});
+overlaySideNavabar.addEventListener("click", function () {
+    sidenavContainer.classList.remove("sidenav_container_active");
+    overlaySideNavabar.classList.remove("sidenav_container_active");
+    document.body.classList.remove("minimize_siderbar");
+});
 
-
-
-lightDarkmode.addEventListener('click', function () {
-    document.body.classList.toggle('light');
-})
-
-arrowLeft.addEventListener('click', function () {
-    document.body.classList.remove('minimize_siderbar');
-})
-
-
-hamburger.addEventListener('click', function () {
-    document.body.classList.add('minimize_siderbar');
-})
-
-hamburgerPhone.addEventListener('click', function () {
-    sidenavContainer.classList.add('sidenav_container_active');
-    overlaySideNavabar.classList.add('sidenav_container_active');
-})
-overlaySideNavabar.addEventListener('click', function () {
-    sidenavContainer.classList.remove('sidenav_container_active');
-    overlaySideNavabar.classList.remove('sidenav_container_active');
-    document.body.classList.remove('minimize_siderbar');
-})
-
-
-
-const myApi = "6b2dec73b6697866a50cdaef60ccffcb"
+const myApi = "6b2dec73b6697866a50cdaef60ccffcb";
 
 const NowPlaying = async () => {
-    const res = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${myApi}&language=en-US&page=1`)
+    const res = await fetch(
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=${myApi}&language=en-US&page=1`
+    );
     const data = await res.json();
     console.log(data);
     const NowPlayingmovies = data.results;
     return NowPlayingmovies;
-}
+};
 
 //<a class="posterlink" href="./movieDetail.html"></a>//
 const NowPlayingfun = (movie) => {
     let url = "./movieDetail.html?id=" + encodeURIComponent(movie.id);
     return `<div class="Now_playing_movies" >
-    <a class="posterlink" href=${url}> <img class="poster" data-id="${movie.id}" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="${movie.title}"></a>
+    <a class="posterlink" href=${url}> <img class="poster" data-id="${movie.id
+        }" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="${movie.title
+        }"></a>
          <p class="movie_title">${movie.title}</p>
          <div class="date_rating">
-             <p class="date">${dateFormatter(movie.release_date)}</p><span class="dot dot2"></span>
-             <p class="rating">${movie.vote_average}<span><svg xmlns="http://www.w3.org/2000/svg" width="10"
+             <p class="date">${dateFormatter(
+            movie.release_date
+        )}</p><span class="dot dot2"></span>
+             <p class="rating">${movie.vote_average
+        }<span><svg xmlns="http://www.w3.org/2000/svg" width="10"
                          height="10" fill="Yellow" class="star bi-star-fill" viewBox="0 0 16 16">
                          <path
                              d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
                      </svg></span></p>
              <div class="category">Movie</div>
              </div>
-         </div>`
-}
+         </div>`;
+};
 
 const dateFormatter = function (date) {
     let currdate = date;
-    const newDate = currdate.slice(0, 4)
+    const newDate = currdate.slice(0, 4);
     return newDate;
-}
+};
 
-
-
-NowPlaying().then(movies => {
-    movies.forEach(moviee => {
+NowPlaying().then((movies) => {
+    movies.forEach((moviee) => {
         const htmll = NowPlayingfun(moviee);
-        NowPlayingMoviesDiv.insertAdjacentHTML("beforeend", htmll)
+        NowPlayingMoviesDiv.insertAdjacentHTML("beforeend", htmll);
+    });
 
-    })
-
-    const NowPlayingMovies = document.querySelectorAll('.Now_playing_movies');
-    NowPlayingMovies.forEach((ele, i) => (ele.style.transform = `TranslateX(${i * 122}%)`));
-
-})
-
-
+    const NowPlayingMovies = document.querySelectorAll(".Now_playing_movies");
+    NowPlayingMovies.forEach(
+        (ele, i) => (ele.style.transform = `TranslateX(${i * 122}%)`)
+    );
+});
 
 // MOVIES SLIDER
 
@@ -99,49 +93,44 @@ let currSlide = 0;
 const maxSlide = 20;
 
 const gotoSlide = function (slides) {
-    const NowPlayingMovies = document.querySelectorAll('.Now_playing_movies');
-    NowPlayingMovies.forEach((ele, i) => (ele.style.transform = `TranslateX(${122 * (i - slides)}%)`));
-
-}
+    const NowPlayingMovies = document.querySelectorAll(".Now_playing_movies");
+    NowPlayingMovies.forEach(
+        (ele, i) => (ele.style.transform = `TranslateX(${122 * (i - slides)}%)`)
+    );
+};
 
 const nextSlide = function () {
     if (currSlide === maxSlide - 6) {
-        rightarrow.classList.add('disablemouse')
-        leftArrow.classList.remove('disablemouse')
+        rightarrow.classList.add("disablemouse");
+        leftArrow.classList.remove("disablemouse");
         //currSlide = 0;
-    }
-    else {
-        rightarrow.classList.remove('disablemouse')
-        leftArrow.classList.remove('disablemouse')
-        currSlide++
+    } else {
+        rightarrow.classList.remove("disablemouse");
+        leftArrow.classList.remove("disablemouse");
+        currSlide++;
     }
     gotoSlide(currSlide);
-}
-
+};
 
 const prevSlide = function () {
     if (currSlide === 0) {
-        rightarrow.classList.remove('disablemouse')
-        leftArrow.classList.add('disablemouse')
+        rightarrow.classList.remove("disablemouse");
+        leftArrow.classList.add("disablemouse");
         /* currSlide = maxSlide - 6;*/
-    }
-    else {
-        leftArrow.classList.remove('disablemouse')
-        rightarrow.classList.remove('disablemouse')
+    } else {
+        leftArrow.classList.remove("disablemouse");
+        rightarrow.classList.remove("disablemouse");
         currSlide--;
     }
-    gotoSlide(currSlide)
+    gotoSlide(currSlide);
 };
 
-leftArrow.addEventListener('click', prevSlide);
-rightarrow.addEventListener('click', nextSlide);
-
-
+leftArrow.addEventListener("click", prevSlide);
+rightarrow.addEventListener("click", nextSlide);
 
 /* MOVIE CLCIKED*/
 
 const html2 = function (movie) {
-
     return ` <div class="movie_details">
     <img class="movie_details_poster" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="">
     <div class="movie_details_about">
@@ -170,5 +159,5 @@ const html2 = function (movie) {
             </svg>Play</button>
     </div>
 
-</div> `
-}
+</div> `;
+};
